fcd1e8afad5fc7c798240b44a1069239ca48111e
Rabbit <imsfc9@gmail.com>
2023-08-24 01:47:12 +0800

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
