21940536a95dc45c4c5ec7447dfcd31bd241dcd3
Rabbit <imsfc9@gmail.com>
2023-08-23 22:42:15 +0800

    chore: update prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -4,16 +4,8 @@ export default {
   semi: false,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: 'es5',
-  useTabs: false,
   plugins: ['prettier-plugin-astro'],
   overrides: [
-    {
-      files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
-      options: {
-        useTabs: false,
-      },
-    },
     {
       files: '*.astro',
       options: {
