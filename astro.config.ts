6a5ad348d4beec12ecf2a94971fccbd23259d98a
Rabbit <imsfc9@gmail.com>
2023-08-23 23:45:56 +0800

    chore: add @astrojs/sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
@@ -6,4 +7,5 @@ export default defineConfig({
   build: {
     assets: 'assets',
   },
+  integrations: [sitemap()],
 })
